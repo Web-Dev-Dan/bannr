04730176fe6cb349df3990979348e09146447f25
Dan <80458000+Web-Dev-Dan@users.noreply.github.com>
2022-06-05 00:48:46 +0900

    Added page switching functionality

--- script.js
+++ script.js
@@ -20,6 +20,12 @@ currentDateText.textContent = currentYear;
 
 
 // ----- 📄 Select Page 📄 -----
+// Pages:
+const homePage = document.getElementById('homePage');
+const bannersPage = document.getElementById('bannersPage');
+const followersPage = document.getElementById('followersPage');
+const imagePage = document.getElementById('imagePage');
+
 // Navbar buttons:
 const homePageBtn = document.getElementById('homePageBtn');
 const bannersPageBtn = document.getElementById('bannersPageBtn');
@@ -35,31 +41,61 @@ const openHomePage = () => {
     bannersPageBtn.classList.remove('btn-selected');
     followersPageBtn.classList.remove('btn-selected');
     imagesPageBtn.classList.remove('btn-selected');
+
+    homePage.classList.remove('element-hidden');
+    bannersPage.classList.add('element-hidden');
+    followersPage.classList.add('element-hidden');
+    imagePage.classList.add('element-hidden');
+
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 }
 
 const openBannersPage = () => {
     bannersPageBtn.classList.add('btn-selected');
     followersPageBtn.classList.remove('btn-selected');
     imagesPageBtn.classList.remove('btn-selected');
+
+    homePage.classList.add('element-hidden');
+    bannersPage.classList.remove('element-hidden');
+    followersPage.classList.add('element-hidden');
+    imagePage.classList.add('element-hidden');
+
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 }
 
 const openFollowersPage = () => {
     bannersPageBtn.classList.remove('btn-selected');
     followersPageBtn.classList.add('btn-selected');
     imagesPageBtn.classList.remove('btn-selected');
+
+    homePage.classList.add('element-hidden');
+    bannersPage.classList.add('element-hidden');
+    followersPage.classList.remove('element-hidden');
+    imagePage.classList.add('element-hidden');
+
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 }
 
 const openImagesPage = () => {
     bannersPageBtn.classList.remove('btn-selected');
     followersPageBtn.classList.remove('btn-selected');
     imagesPageBtn.classList.add('btn-selected');
+
+    homePage.classList.add('element-hidden');
+    bannersPage.classList.add('element-hidden');
+    followersPage.classList.add('element-hidden');
+    imagePage.classList.remove('element-hidden');
+
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 }
 
+// Navbar Event Listeners:
 homePageBtn.addEventListener('click', openHomePage);
 bannersPageBtn.addEventListener('click', openBannersPage);
 followersPageBtn.addEventListener('click', openFollowersPage);
 imagesPageBtn.addEventListener('click', openImagesPage);
 
+// Options Boxes Event Listeners:
 makeBannerBtn.addEventListener('click', openBannersPage);
 celebrateFollowersBtn.addEventListener('click', openFollowersPage);
 makeImageBtn.addEventListener('click', openImagesPage);
